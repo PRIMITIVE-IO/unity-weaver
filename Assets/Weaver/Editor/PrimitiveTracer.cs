9591d19e582aa895fd6abe5541a841e091c16dc0
John <john@primitive.io>
2022-09-09 10:00:47 -0400

    add instance id generator for non-monobehaviour classes

--- Assets/Weaver/Editor/PrimitiveTracer.cs
+++ Assets/Weaver/Editor/PrimitiveTracer.cs
@@ -1,20 +1,35 @@
+using System.Runtime.Serialization;
 using UnityEngine;
 
 namespace Weaver.Editor
 {
     public static class PrimitiveTracer
     {
+        static readonly ObjectIDGenerator objectIDGenerator = new();
+        
         public static void Trace(object traceObject, string methodName)
         {
-            Debug.Log(methodName);
+            string output = $"Method Name: {methodName}";
+            bool isNotUnityManaged = true;
             if (traceObject is Object traceObjectObject)
             {
-                Debug.Log(traceObjectObject.GetInstanceID());
+                isNotUnityManaged = false;
+                output += $" Object ID: {traceObjectObject.GetInstanceID()}";
             }
+            
             if (traceObject is Component traceObjectComponent)
             {
-                Debug.Log(traceObjectComponent.gameObject.GetInstanceID());
+                isNotUnityManaged = false;
+                output += $" GameObject ID: {traceObjectComponent.gameObject.GetInstanceID()}";
             }
+
+            if (isNotUnityManaged)
+            {
+                long classInstanceId = objectIDGenerator.GetId(traceObject, out bool firstTime);
+                output += $" Instance ID {classInstanceId}";
+            }
+            
+            Debug.Log(output);
         }
     }
 }
